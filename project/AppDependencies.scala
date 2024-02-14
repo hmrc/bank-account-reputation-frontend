d26e785e23b85df8fc7c10f9fb8a1013eac01b2f
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2024-02-14 08:11:02 +0000

    CIR-1444: Update play-hmrc for new crown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,15 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapPlayVersion = "8.1.0"
+  private val bootstrapPlayVersion = "8.4.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.2.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapPlayVersion % Test,
-    "org.jsoup" % "jsoup" % "1.13.1" % Test
+    "org.jsoup" % "jsoup" % "1.15.4" % Test
   )
 }
