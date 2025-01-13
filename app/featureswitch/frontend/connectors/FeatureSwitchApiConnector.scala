ebc5287e2d558fd8a7b87f21d53eaf2c7556a7b0
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2025-01-13 10:17:00 +0000

    CIR-1858 Feature toggle UI for backend

---
+++ app/featureswitch/frontend/connectors/FeatureSwitchApiConnector.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package featureswitch.frontend.connectors
+
+import featureswitch.core.models.FeatureSwitchSetting
+import play.api.http.Status.OK
+import play.api.libs.json.{JsError, JsSuccess, Json, Reads}
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class FeatureSwitchApiConnector @Inject()(httpClient: HttpClientV2)(implicit ec: ExecutionContext) {
+
+  def retrieveFeatureSwitches(featureSwitchProviderUrl: String
+                             )(implicit reads: Reads[Seq[FeatureSwitchSetting]],
+                               hc: HeaderCarrier): Future[Seq[FeatureSwitchSetting]] = {
+    httpClient.get(url"$featureSwitchProviderUrl").execute.map(
+      response =>
+        response.status match {
+          case OK =>
+            response.json.validate[Seq[FeatureSwitchSetting]] match {
+              case JsSuccess(settings, _) => settings
+              case JsError(errors) => throw new Exception(errors.head.toString)
+            }
+          case _ => throw new Exception(s"Could not retrieve feature switches from $featureSwitchProviderUrl")
+        }
+    )
+  }
+
+
+  def updateFeatureSwitches(featureSwitchProviderUrl: String,
+                            featureSwitchSettings: Seq[FeatureSwitchSetting]
+                           )(implicit hc: HeaderCarrier): Future[Seq[FeatureSwitchSetting]] = {
+    httpClient.post(url"$featureSwitchProviderUrl").withBody(Json.toJson(featureSwitchSettings)).execute.map {
+      response =>
+        response.status match {
+          case OK =>
+            response.json.validate[Seq[FeatureSwitchSetting]] match {
+              case JsSuccess(settings, _) => settings
+              case JsError(errors) => throw new Exception(errors.head.toString)
+            }
+          case _ => throw new Exception(s"Could not retrieve feature switches from $featureSwitchProviderUrl")
+        }
+    }
+  }
+}
